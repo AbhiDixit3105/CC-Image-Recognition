02baa750228b57b3d2b5094d7af282bb2f3e85c2
Bhavesh Khubnani <bhavesh.khubnani@gmail.com>
2023-10-01 14:47:10 -0700

    ICC-13: Adding template code for WebServer.py

---
+++ web-server/WebServer.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    pass
+def create_ec2_instance():
+    pass
+def destroy_ec2_instance():
+    pass
+def send_to_sqs():
+    pass
+def receive_from_sqs():
+    pass
+
+def scale_in_function():
+    pass
+def scale_out_function():
+    pass
