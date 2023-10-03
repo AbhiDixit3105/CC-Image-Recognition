0d872a62433f4c5330f127ecd9e8f209312dda98
Bhavesh Khubnani <bhavesh.khubnani@gmail.com>
2023-10-03 12:48:34 -0700

    ICC-13: adding flask app

--- app-tier/SqsController.py
+++ app-tier/SqsController.py
@@ -1,18 +1,21 @@
+import subprocess
+
 import boto3
 
-# get client
-# region_name='us-east-1',aws_access_key_id="ACCESS_KEY_HERE",
-client = boto3.client('sqs')
-#   aws_secret_access_key="SECRET_KEY_HERE")
-
-# SQS Queue URL
 request_queue_url = 'https://sqs.us-east-1.amazonaws.com/827983923224/cc-proj-1-request-queue'
 response_queue_url = 'https://sqs.us-east-1.amazonaws.com/827983923224/cc-proj-1-response-queue'
 
+access_key = 'AKIA4BR5QBAMDUC72SQX'
+secret_key = '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'
+s3_bucket_in = 'cc-ss-input-bucket'
+s3_bucket_o = 'cc-ss-output-bucket'
 
-# get first available message in recieve queue
+sqs_client = boto3.client('sqs', region_name="us-east-1", aws_access_key_id=access_key,
+                          aws_secret_access_key=secret_key)
+
+
 def get_queue_data():
-    request = client.receive_message(
+    request = sqs_client.receive_message(
         QueueUrl=request_queue_url,
         AttributeNames=['SentTimestamp'],
         MaxNumberOfMessages=1,
@@ -24,15 +27,36 @@ def get_queue_data():
     message = request['Messages'][0]
     receipt_handle = message['ReceiptHandle']
 
-# send output data to response queue
 
-
-def send_data_to_queue(image_ouput):
-    response = client.send_message(
+def send_data_to_queue(image_output):
+    response = sqs_client.send_message(
         QueueUrl=response_queue_url,
         MessageBody=image_output
     )
 
 
-def delete_message(message):
-    message.delete()
+def download_image(path):
+    session = boto3.session.Session(aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+    s3_resource = session.resource("s3")
+    file_name = '/home/ubuntu/input/' + image_name + '.jpg'
+    s3_resource.meta.client.download_file(s3_bucket_in, image_name, file_name)
+
+
+def classify_image(image_name):
+    path = '/home/ubuntu/input/' + image_name + '.jpg'
+    filename = '/home/ubuntu/output/' + image_name + '.txt'
+    subprocess.run(['touch', filename])
+    output_file = open(filename, "w")
+    subprocess.run(('python3', './image_classification.py', path), stdout=output_file)
+    pass
+
+
+if __name__ == '__main__':
+    while True:
+        message = get_queue_data()
+        image_name = message.body()
+        download_image(image_name)
+        classify_image(image_name)
+        send_data_to_queue(image_name + "hello_world")
+        message.delete()
+        # time.sleep(2)
--- web-tier/Controllers/AwsController.py
+++ web-tier/Controllers/AwsController.py
@@ -6,8 +6,9 @@ class AwsController:
         self.kms_key_id = 'b9279eb0-0de3-4f49-96df-5d7467abdd5e'
         self.encryption_key = 'cc-serverless-server'
         self.sqs_client = boto3.client('sqs')
-        self.request_queue_url = ''
-        self.response_queue_url = ''
+        self.request_queue_url = 'https://sqs.us-east-1.amazonaws.com/827983923224/cc-proj-1-request-queue'
+        self.response_queue_url = 'https://sqs.us-east-1.amazonaws.com/827983923224/cc-proj-1-response-queue'
+
 
     def upload_to_s3(self, file, bucket_name):
         s3 = boto3.client('s3')
@@ -22,33 +23,12 @@ class AwsController:
         pass
 
     def send_to_sqs(self, message):
-        pass
-
-    def receive_from_sqs(self):
-        self.sqs_client.receive_message(
+        sqs_client = boto3.client('sqs', region_name="us-east-1")
+        response = sqs_client.send_message(
             QueueUrl=self.request_queue_url,
-            AttributeNames=[
-                'All' ],
-            MessageAttributeNames=[
-                'string',
-            ],
-            MaxNumberOfMessages=10,
-            VisibilityTimeout=3,
-            WaitTimeSeconds=3
+            MessageBody=message
         )
-        pass
+        return response
 
-    def execute_cmd_on_ec2(self, client, commands, instance_ids):
-        """Runs commands on remote linux instances
-        :param client: a boto/boto3 ssm client
-        :param commands: a list of strings, each one a command to execute on the instances
-        :param instance_ids: a list of instance_id strings, of the instances on which to execute the command
-        :return: the response from the send_command function (check the boto3 docs for ssm client.send_command() )
-        """
 
-        resp = client.send_command(
-            DocumentName="AWS-RunShellScript",  # One of AWS' preconfigured documents
-            Parameters={'commands': commands},
-            InstanceIds=instance_ids,
-        )
-        return resp
+
