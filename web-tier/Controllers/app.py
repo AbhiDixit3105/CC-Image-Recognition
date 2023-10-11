b22391597c93219924e33bbb60949371df92a822
Bhavesh Khubnani <bhavesh.khubnani@gmail.com>
2023-10-10 23:35:23 -0700

    ICC-13:  improving html

--- web-tier/Controllers/app.py
+++ web-tier/Controllers/app.py
@@ -26,7 +26,7 @@ logging.basicConfig(filename='app.log', level=logging.DEBUG, format='%(asctime)s
 # i-02d039e7f2ee6aa41
 
 def allowed_file(filename):
-    allowed_extensions = {'png', 'jpg', 'jpeg', 'gif'}
+    allowed_extensions = {'png', 'jpg', 'jpeg'}
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
 
 
